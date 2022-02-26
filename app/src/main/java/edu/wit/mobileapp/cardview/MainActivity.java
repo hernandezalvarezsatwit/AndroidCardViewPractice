ce8c2efec504bc23d604c04e900bba4505cf8d80
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-02-26 01:00:27 -0500

    Done

--- app/src/main/java/edu/wit/mobileapp/cardview/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/cardview/MainActivity.java
@@ -1,12 +1,20 @@
 package edu.wit.mobileapp.cardview;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+/**
+ * This application uses CardView to display cards with an image, title and description
+ * This is the MainActivity it creates the items that will be shown and calls necessary
+ * classes to display them.
+ * @author Samuel Hernandez
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,12 +22,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<ListElement> list = new ArrayList<>();
-        for(int i = 0; i < 12; i++)
-            list.add(new ListElement("#775447" , "Pedro", "Mexico"));
+        //Get placeholder default image
+        Bitmap defaultImage = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
 
+        //Create list and populate it
+        String description = "This is just a placeholder for an actual description";
+        List<Card> cards = IntStream.range(1, 12).mapToObj(i ->
+                new Card(defaultImage, "Title "+i, description)).collect(Collectors.toList());
+
+        //Create manager, adapter, pass the list of cards
         LinearLayoutManager manager = new LinearLayoutManager(this);
-        ListAdapter adapter = new ListAdapter(list, this);
+        CardAdapter adapter = new CardAdapter(cards, this);
+        //Create recycler view and set manager and adapter
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
